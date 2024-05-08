c7ff7fc042928ac97385a1d296e4dc3e0dd398e2
S1TO01 <kamil.sitko01@gmail.com>
2024-05-08 20:56:04 +0200

    First commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Stanowiska_kasowego_w_sklepie
+{
+    internal class Klient
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace Projekt_Stanowiska_kasowego_w_sklepie
+{
+    internal class Program
+    {
+        static Dictionary<string, (string, int)> users = new Dictionary<string, (string, int)>();
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("1. Zarejestruj klienta");
+                Console.WriteLine("2. Logowanie klienta");
+                Console.WriteLine("3. Wyjście");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Register();
+                        break;
+                    case "2":
+                        Login();
+                        break;
+                    case "3":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+                        break;
+                }
+
+            }
+             static void Register()
+            {
+                Console.WriteLine("Podaj nazwę użytkownika");
+                string username = Console.ReadLine();
+                Console.WriteLine("Podaj hasło");
+                string password = Console.ReadLine();
+                Console.WriteLine("Podaj numer pesel");
+                int ID = Convert.ToInt32(Console.ReadLine());
+
+                if (!users.ContainsKey(username))
+                {
+                    users.Add(username, (password, ID));
+                    Console.WriteLine("Rejestracja pomyślna!");
+                }
+                else
+                {
+                    Console.WriteLine("Taki użytkownik już istnieje");
+                }
+
+            }     
+        }
+    }
+}
